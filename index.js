97c0b4038871f1d1b93bab2c64f692223086e1aa
debe123d <43981524+debe123d@users.noreply.github.com>
2020-10-29 11:40:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ const express = require('express');
 const app = express();
 var port = process.env.PORT || 3000;
 app.get('/',(req,res)=>res.send("Debendra sahu my code is cossuss i am very happy"));
+app.get('/api', (req,res) => {
+    res.json({ message: 'Hello World'});
+});
 app.listen(port,()=> console.log("port:" + port));
